940f6c34d29316a0bbc9a1ec2dc7f9929de64954
Jonathan Quino <jonathan.quino.26793@gmail.com>
2022-01-04 16:29:19 -0600

    Se añade minima funcion de javascript

---
+++ Assets/js/app.js
@@ -0,0 +1,5 @@
+function formulariolleno(){
+    var nombre = document.getElementById("inputnombre");
+    var mail = document.getElementById("inputmail");
+    if(nombre.value.length!=0 && mail.value.length!=0) alert("Gracias por llenar el formulario")
+}
